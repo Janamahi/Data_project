eced5e3b92b7cfd1110496c657083eaacaeba899
Janamahima P <125370559+Janamahi@users.noreply.github.com>
2023-03-31 16:44:51 +0000

    Completed the program with mentioned features

--- main.py
+++ main.py
@@ -1,57 +1,111 @@
-# Imports required ---
+#Imports required ---
 import streamlit as st
 import sqlite3 as sqlite
 import pandas as pd
+#import altair as alt
+import plotly.express as px
 
 ## Instructions:
-#Go in shell
-#write "pip install--upgrade streamlit"
-#launch with "streamlit run main.py"
+# Go in Shell (on the right)
+# write "pip install --upgrade streamlit"
+# launch with "streamlit run main.py"
 
 
 @st.cache_data
+# Fetching data
 def get_data():
-  
   con = sqlite.connect("Fake_sales_data.db")
-  data = pd.read_sql_query("SELECT * from SalesA", con)
+  data = pd.read_sql_query("SELECT * from  SalesA", con)
   return data
 
+
 data = get_data()
+data['company'] = data['company'].astype('string')
+data['cat'] = data['cat'].astype('string')
 
 st.title('Welcome to the Streamlit Tour! 🎈')
 with st.expander("About this App"):
   st.write("""
-         This app will introduce you to the Streamlit Library which
-         helps to build and deploy data driven web apps with ease using Python. 😉
-     """)
+   This app will introduce you to the Streamlit Library which
+   helps to build and deploy data driven web apps with ease using Python. 😉
+   """)
+
 st.title('Sales Dashboard')
+st.write("Analyisng the sales data of companies in SalesA dataset")
+st.write(f"We have {len(data)} datapoints")
 
-st.write(f"we have {len(data)} datapoints")
+# Selection of Company and sum of total sales
+company = st.selectbox("Select a Company", data["company"].unique(), index=0)
 
-#1. select company and sum of the sales
-choice = st.selectbox("Select a company", data["company"].unique(), index=0)
-
-new_data = data[data["company"] == choice]
-st.write(new_data.head())
-st.write(f"Sum of sales {choice}:")
+new_data = data[data["company"] == company]
+st.write(f"Total sum of sales for {company}:")
 st.write(new_data["price"].sum())
 
-#A way to select demographic based data
-ppl = st.selectbox("Select a category", new_data["cat"].unique(), index=0)
-new_data = new_data[new_data["cat"] == ppl]
-st.write(ppl)
+# Category selection
+category = st.multiselect("Select a Category",
+                          data["cat"].unique(),
+                          default="Young")
 
-#Multiple selection of the category
-#ppl = st.multiselect("Select a category", ['Young','Active','Retired'])
-#new_data = new_data[['Young','Active','Retired'] == ppl]
-#st.write(ppl)
+# Data filtering
+data_selection = data.query("company == @company & cat == @category")
 
-#print the sum of price per week
-st.write(f"Sum of sales on weekly basis {choice}:")
-perweek = new_data.groupby("week")["price"].sum()
-st.write(perweek)
+# 1. Differnt for Products Sold data and chart
+st.write(f"Products Sold for {company} of {category} category:")
 
-#plot the sales through time
-st.write(f"Plot of sales by time {choice}:")
-st.line_chart(perweek, y="price")
-#st.write(data)
+st.write(f"We have {len(data_selection)} datapoints")
+
+st.write(" Category and price range of different products:")
+prod_sold = data_selection[['cat', 'price']].value_counts().reset_index()
+st.write(prod_sold.groupby(["cat"])["price"].unique())
+st.write(prod_sold.head())
+
+#Visualisation
+prod_sold.columns = [*prod_sold.columns[:-1], 'count']
+
+plot_bar = px.bar(
+  prod_sold,
+  x='price',
+  y='count',
+  color='cat',
+  title='<b> Analysis of different products sold using bar chart </b>')
+
+st.plotly_chart(plot_bar)
+
+# Volume of Sales
+
+st.write(f"Volume of Sales for {company} in {category} category :")
+volume_sales = data_selection["price"].count()
+st.write(volume_sales)
+
+# Revenue
+
+st.write(f"Revenue for {company} in {category} category :")
+data_selection['price'] = data['price'].astype('float')
+revenue = data_selection["price"].sum()
+st.write(revenue)
+
+# Volume of sales per week
+volume_per_week = data_selection.groupby(["week"
+                                          ])["price"].count().reset_index()
+
+# The revenues per week
+sum_per_week = data_selection.groupby(["week"])["price"].sum().reset_index()
+
+# Plot volume & revenue through time
+
+st.write(" Plotting Volume & Revenue through Time:")
+tab1, tab2 = st.tabs(["Volume", "Revenue"])
+
+with tab1:
+  figvol = px.line(volume_per_week,
+                   x="week",
+                   y="price",
+                   title='Volume through time')
+  st.plotly_chart(figvol)
+
+with tab2:
+  figrev = px.line(sum_per_week,
+                   x="week",
+                   y="price",
+                   title='Revenue through time')
+  st.plotly_chart(figrev)
